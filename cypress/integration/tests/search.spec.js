1e8bd09952e4d25a44e82e6f6eb2a8ffa3d8f593
CARLA MILENIS GÓMEZ PÉREZ <carlagomezp1@gmail.com>
2021-06-22 00:40:02 -0400

    Add search by using the search bar

--- cypress/integration/tests/search.spec.js
+++ cypress/integration/tests/search.spec.js
@@ -2,16 +2,23 @@
 
 import Search from '../../pageObjects/searchPage'
 
+const search = new Search()
+
+beforeEach(() => {
+    cy.visit('http://automationpractice.com/index.php')
+})
+
 describe('Search using navigation', () => {
-    const search = new Search()
 
-    before(() => {
-        cy.visit('http://automationpractice.com/index.php')
-    })
-
-    it('should take the user to Evening dresses', () => {
+    it('should take the user to Evening dresses using the navigation', () => {
         search.getCategory().eq(0).invoke('show')
         search.getSubcategory().click()
     })
+})
 
+describe('Search using the search bar', () =>{
+    
+    it('should seach for the item that the user enters', () => {
+        search.getSearchBar().type('Blouse{Enter}')
+    })
 })
--- cypress/pageObjects/searchPage.js
+++ cypress/pageObjects/searchPage.js
@@ -8,6 +8,9 @@ class Search {
         return cy.get('.sf-menu li li li a').contains('Evening');
     }
 
+    getSearchBar(){
+        return cy.get('#search_query_top');
+    }
 }
 
 export default Search
