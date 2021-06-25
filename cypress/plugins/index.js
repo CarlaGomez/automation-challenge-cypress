d8a89c5689b65132fb80edebdac7fd0226d2ba00
CARLA MILENIS GÓMEZ PÉREZ <carlagomezp1@gmail.com>
2021-06-25 01:23:51 -0400

    Add reports with allure

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+/// <reference types="@shelex/cypress-allure-plugin" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -16,7 +17,10 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+    allureWriter(on, config);
+    return config;
+};
